21c6bbd642caf8bea754c9dcc0da971aa5ce660f
Joshua Owusu <joshbillion1@gmail.com>
2023-07-02 12:53:49 +0000

    Commit auto_update

--- Super-Express/layouts/MainLayout.vue
+++ Super-Express/layouts/MainLayout.vue
@@ -1,37 +1,37 @@
 <template>
   <div id="MainLayout" class="w-full fixed z-50">
-    <div id="TopMenu" class="w-full bg-[#fafafa] border-b md:block hidden">
+    <div id="TopMenu" class="w-full bg-[#FAFAFA] border-b md:block hidden">
       <ul
-        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#fafafa] max-w-[1200px]"
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
       >
         <li
-          class="border-r border-r-gray-400 px-3 hover:text-[#ff4646] cursor-pointer"
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
         >
-          Sell On AliExpress
+          Sell on AliExpress
         </li>
         <li
-          class="border-r border-r-gray-400 px-3 hover:text-[#ff4646] cursor-pointer"
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
         >
           Cookie Preferences
         </li>
         <li
-          class="border-r border-r-gray-400 px-3 hover:text-[#ff4646] cursor-pointer"
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
         >
           Help
         </li>
         <li
-          class="border-r border-r-gray-400 px-3 hover:text-[#ff4646] cursor-pointer"
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
         >
           Buyer Protection
         </li>
-        <li class="px-3 hover:text-[#ff4646] cursor-pointer">
+        <li class="px-3 hover:text-[#FF4646] cursor-pointer">
           <Icon name="ic:sharp-install-mobile" size="17" />
           App
         </li>
         <li
           @mouseenter="isAccountMenu = true"
           @mouseleave="isAccountMenu = false"
-          class="relative flex items-center px-2.5 hover:text-[#ff4646] h-full cursor-pointer"
+          class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
           :class="
             isAccountMenu
               ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
@@ -47,15 +47,16 @@
             v-if="isAccountMenu"
             class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
           >
-            <div v-if="true">
+            <div v-if="!user">
               <div class="text-semibold text-[15px] my-4 px-3">
-                Welcome to I-Express!
+                Welcome to AliExpress!
               </div>
               <div class="flex items-center gap-1 px-3 mb-3">
                 <NuxtLink
                   to="/auth"
-                  class="bg-[#ff4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
-                  >Login / Register
+                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                >
+                  Login / Register
                 </NuxtLink>
               </div>
             </div>
@@ -67,7 +68,6 @@
               >
                 My Orders
               </li>
-
               <li
                 v-if="user"
                 @click="client.auth.signOut()"
