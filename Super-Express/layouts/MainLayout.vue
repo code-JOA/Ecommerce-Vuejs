6a2ec608382e5a720161e4ba46b7f192acb07ad5
Joshua Owusu <joshbillion1@gmail.com>
2023-07-02 13:03:00 +0000

    Commit auto_update

--- Super-Express/layouts/MainLayout.vue
+++ Super-Express/layouts/MainLayout.vue
@@ -84,7 +84,11 @@
     <div id="MainHeader" class="flex items-center w-full bg-white">
       <div
         class="flex lg:justify-star justify-between gap-10 max-w-[1150px] w-full px-3 py-5 max-auto"
-      ></div>
+      >
+        <NuxtLink to="/" class="min-w-[170px]">
+          <img width="170" src="/AliExpress-logo.png" />
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
