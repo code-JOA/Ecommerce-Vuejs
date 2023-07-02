ce0d6dadb8f788eddeb1cd06fdc492e35d0f51e1
Joshua Owusu <joshbillion1@gmail.com>
2023-07-02 10:22:00 +0000

    Commit auto_update

--- Super-Express/pages/index.vue
+++ Super-Express/pages/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="bg-red-400">Test</div>
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+  </MainLayout>
 </template>
 
-<script setup></script>
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+</script>
