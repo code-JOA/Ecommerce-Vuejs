a717155c8d49e4393bb4dea0f66d4e14425e701c
Joshua Owusu <joshbillion1@gmail.com>
2023-07-02 10:35:13 +0000

    Commit auto_update

--- Super-Express/layouts/MainLayout.vue
+++ Super-Express/layouts/MainLayout.vue
@@ -1,5 +1,9 @@
 <template>
-  <div></div>
+  <div id="MainLayout" class="w-full fixed z-50">
+    <div id="TopMenu" class="w-full bg-[#fafafa] border-b md:block hidden">
+      <ul class="flex items-center"></ul>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
