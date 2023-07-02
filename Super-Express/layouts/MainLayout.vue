0cac248d2889b547d949d5fb7f0a73476d14054d
Joshua Owusu <joshbillion1@gmail.com>
2023-07-02 12:36:58 +0000

    Commit auto_update

--- Super-Express/layouts/MainLayout.vue
+++ Super-Express/layouts/MainLayout.vue
@@ -41,6 +41,25 @@
           <Icon name="ph:user-thin" size="17" />
           Account
           <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+
+          <div
+            id="AccountMenu"
+            v-if="isAccountMenu"
+            class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          >
+            <div v-if="true">
+              <div class="text-semibold text-[15px] my-4 px-3">
+                Welcome to I-Express!
+              </div>
+              <div class="flex items-center gap-1 px-3 mb-3">
+                <NuxtLink
+                  to="/auth"
+                  class="bg-[#ff4646] text-center w-full text-[16px] rounded-sm"
+                  >Login / Register
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
