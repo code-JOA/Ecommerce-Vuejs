bfd1fcfdc31423147313c29b5b84d555bc97d356
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 11:59:50 +0000

    Commit auto_update

--- Super-Express/pages/index.vue
+++ Super-Express/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <MainLayout>
-    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div></div>
+    </div>
   </MainLayout>
 </template>
 
