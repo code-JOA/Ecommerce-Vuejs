95371b221f18f9fadb906f5973e69bb80ad23721
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 11:38:56 +0000

    Commit auto_update

--- Super-Express/components/Footer.vue
+++ Super-Express/components/Footer.vue
@@ -1,19 +1,25 @@
 <template>
-
-<div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
     <div class="max-w-[1200px] mx-auto px-4">
-
-        <div class="md:flex gap-4 justify-between w-full">
-
-            <div class="md:w-[50%] pt-4">
-
-                
-
-            </div>
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-lg font-semibold text-[#282828]">Help</div>
+          <div class="text-[#999999] text-sm">
+            Help Center, Disputes & Reports, Buyer Protection, Report IPR
+            infringement, Regulated Information
+          </div>
         </div>
 
+         <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">AliExpress Multi-Language Sites</div>
+                    <div class="text-[#999999] text-sm">Russian, Portuguese, Spanish, French, German, Italian, Dutch, Turkish, Japanese, Korean, Thai, Vietnamese, Arabic, Hebrew, Polish</div>
+                </div>
+            </div>
+
+            
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
