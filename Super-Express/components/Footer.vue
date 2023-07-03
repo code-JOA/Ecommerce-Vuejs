d64dc449e0fef0c3163a5995c327245f5338c749
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 10:51:28 +0000

    Commit auto_update

--- Super-Express/components/Footer.vue
+++ Super-Express/components/Footer.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
-        <div class="max-w-[1200px] mx-auto px-4"></div>
+
+<div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+
+        <div class="md:flex gap-4 justify-between w-full">
+
+            <div >
+
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script setup>
