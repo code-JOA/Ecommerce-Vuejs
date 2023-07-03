44bbc5ffc182453de56588bd452feeccb746f847
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 12:09:06 +0000

    Commit auto_update

--- Super-Express/pages/index.vue
+++ Super-Express/pages/index.vue
@@ -14,4 +14,26 @@
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
+
+let products = [
+  {id:1, title: 'Title 1', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+   {id:2, title: 'Title 2', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+    {id:3, title: 'Title 3', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+     {id:4, title: 'Title 4', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+      {id:5, title: 'Title 5', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+ {id:6, title: 'Title 6', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+  {id:7, title: 'Title 7', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+ {id:8, title: 'Title 8', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+ {id:9, title: 'Title 9', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+ {id:10, title: 'Title 10', description: 'This is a description', url:'https:/picsum.photos/id/7/800/800', price: 9999 }
+
+
+]
 </script>
