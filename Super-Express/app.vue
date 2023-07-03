0cafb446729a4117728b4c1b1a273f75f4039686
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 14:48:04 +0000

    Commit auto_update

--- Super-Express/app.vue
+++ Super-Express/app.vue
@@ -4,4 +4,34 @@
   <MenuOverlay />
 </template>
 
-<script setup></script>
+<script setup>
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+
+const route = useRoute();
+
+let windowWidth = ref(process.client ? window.innerWidth : "");
+
+onMounted(() => {
+  userStore.isLoading = true;
+  window.addEventListener("resize", function () {
+    windowWidth.value = window.innerWidth;
+  });
+});
+
+watch(
+  () => windowWidth.value,
+  () => {
+    if (windowWidth.value >= 767) {
+      userStore.isMenuOverlay = false;
+    }
+  }
+);
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true;
+  }
+);
+</script>
