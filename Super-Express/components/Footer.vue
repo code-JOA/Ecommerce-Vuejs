c25f3629198772f7ff087c009793e596169d9293
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 10:49:23 +0000

    Commit auto_update

--- Super-Express/components/Footer.vue
+++ Super-Express/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+        <div class="max-w-[1200px] mx-auto px-4"></div>
 </template>
 
-<script setup></script>
+<script setup>
+</script>
