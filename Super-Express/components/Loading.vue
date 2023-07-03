4e32079b6f04c140e6317c8bae89acd8273f0ca1
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 10:41:59 +0000

    Commit auto_update

--- Super-Express/components/Loading.vue
+++ Super-Express/components/Loading.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div
+    class="fixed bg-black bg-opacity-40 inset-0 w-full z-40 flex items-center justify-center h-[100vh] overflow-hidden"
+  ></div>
 </template>
 
 <script setup></script>
