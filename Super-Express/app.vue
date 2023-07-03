c320664438828f26f99a345bb4a7a513f0be3d29
Joshua Owusu <joshbillion1@gmail.com>
2023-07-03 14:49:10 +0000

    Commit auto_update

--- Super-Express/app.vue
+++ Super-Express/app.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]" />
   <NuxtPage />
-  <MenuOverlay />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out invisible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
 
 <script setup>
